58216ca9eccc60faf41e5f2b296f32778761e824
mfbernasol <mfbernasol@gmail.com>
2024-05-26 22:11:12 -0500

    add gitignore

--- FinShark-API/obj/Debug/net8.0/FinShark-API.AssemblyInfo.cs
+++ FinShark-API/obj/Debug/net8.0/FinShark-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinShark-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cdaab1c5f5acb2addf379d8f1844fba837bb55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c6bfc928254dc2a7eca40e085d67e82c546656")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinShark-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinShark-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
