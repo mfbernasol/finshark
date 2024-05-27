75cbcfc69a9384f96a07fec74c60f626e9aa9c98
mfbernasol <mfbernasol@gmail.com>
2024-05-27 14:21:05 -0500

    init db and GET controller

---
+++ FinShark-API/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+using FinShark_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinShark_API.Data;
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+    {
+        
+    }
+    
+     public DbSet<Stock> Stock { get; set; }
+     public DbSet<Comment> Comments { get; set; }
+     
+}
--- FinShark-API/Models/Stock.cs
+++ FinShark-API/Models/Stock.cs
@@ -4,15 +4,16 @@ namespace FinShark_API.Models
 {
     public class Stock
     {
-        public int  Id { get; set; }
+        public int Id { get; set; }
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
-        [Column(TypeName = "decimal(18,2")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Purchase { get; set; }
-        [Column(TypeName = "decimal(18,2")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
-        public List<Comment> Commments { get; set; } = new List<Comment>();
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
